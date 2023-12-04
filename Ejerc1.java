381cfb0f91bbe2bbb9ac9350af9a63144a409697
Jetch1 <149003679+Jetch1@users.noreply.github.com>
2023-12-04 22:48:24 +0100

    Solucion

---
+++ Ejerc1.java
@@ -0,0 +1,13 @@
+package ejercicios;
+
+/*
+ * Ejercicio 1: Escribe un programa por pantalla que diga tu nombre.
+ */
+
+ public class Ejerc1 {
+
+    public static void main(String[] args) {
+        
+      System.out.println("Escribe aquí tu nombre");
+    }
+ }
